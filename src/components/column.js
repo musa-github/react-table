3203ae171633d186d81b2a9bd6a6409b61ecfb09
Musa <smabumusa98gmail.com>
2021-08-27 19:25:13 +0600

    basic table with header body and footer

---
+++ src/components/column.js
@@ -0,0 +1,35 @@
+export const COLUMNS = [
+      {
+        Header: 'id',
+        Footer:"id",
+        accessor:"id"
+      },
+      {
+        Header: 'first Name',
+        Footer: 'first Name',
+        accessor:"first_name"
+        
+      },
+      {
+        Header: 'last ame',
+        Footer: 'last ame',
+        accessor:"last_name"
+      },
+      {
+        Header: 'Email',
+        Footer: 'Email',
+        accessor:"email"
+      },
+      {
+        Header: 'Phone',
+        Footer: 'Phone',
+        accessor:"phone"
+      },
+      {
+        Header: 'age',
+        Footer: 'age',
+        accessor:"age"
+        
+      },
+    ]
+  
