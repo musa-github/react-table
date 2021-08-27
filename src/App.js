17bb908628bc7f6b099ac603b64dc05c4da25c77
Musa <smabumusa98gmail.com>
2021-08-27 20:37:59 +0600

    sorting table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Table from './components/Table';
+// import Table from './components/Table';
+import Sorting from './components/Sorting';
 function App() {
   return (
     <div className="App">
-      <Table/>
+      {/* <Table/> */}
+      <Sorting/>
     </div>
   );
 }
