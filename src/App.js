c442863fa4416907559efe77ec92684abca92091
Musa <smabumusa98gmail.com>
2021-08-27 23:02:39 +0600

    added pagination fetures

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 // import Table from './components/Table';
 // import Sorting from './components/Sorting';
-import FilteringTable from './components/FilteringTable';
+// import FilteringTable from './components/FilteringTable';
+import Pagination from './components/Pagination';
 function App() {
   return (
     <div className="App">
       {/* <Table/> */}
       {/* <Sorting/> */}
-      <FilteringTable/>
+      <Pagination/>
     </div>
   );
 }
---
+++ src/components/Pagination.js
@@ -0,0 +1,111 @@
+
+import React, { useMemo } from 'react';
+import { useGlobalFilter, usePagination, useSortBy, useTable } from 'react-table';
+import { COLUMNS } from "./column";
+import GlobalFilter from './GlobalFilter';
+import mock_data from "./MOCK_DATA";
+import "./table.css";
+
+ function Pagination() {
+   const data = useMemo(() => mock_data,[])
+ const columns = useMemo(()=>COLUMNS,[])
+  
+ 
+ const tableInstance = useTable({ columns, data },
+  useGlobalFilter
+  /*this hoock for global filtering */,
+  useSortBy/*this hoock for sorting */,
+  usePagination
+  
+  )
+ const {
+   getTableProps,
+   getTableBodyProps,
+   headerGroups,
+   footerGroups,
+//    rows,  this is not needed for pagination
+   prepareRow,
+   // for pagination need to destucture those things
+  page,
+  nextPage,
+  previousPage,
+  canNextPage,
+  canPreviousPage,
+  pageOptions,
+
+   //nothing need to destucture here for sorting
+   state,
+   setGlobalFilter
+   // this two thing have to destucture here
+
+      }=tableInstance
+
+      const {globalFilter,/*for pagination */pageIndex, pageSize}=state
+ 
+   return (
+     <>
+     <GlobalFilter 
+     filter={globalFilter} 
+     setFilter={setGlobalFilter}/>
+     
+     <table {...getTableProps()} >
+       <thead>
+         {headerGroups.map(headerGroup => (
+           <tr {...headerGroup.getHeaderGroupProps()}>
+             {headerGroup.headers.map(column => (
+               <th
+                 {...column.getHeaderProps(column.getSortByToggleProps())}
+                
+               >
+                 {column.render('Header')}
+                 <span> {column.isSorted?(column.isSortedDesc? "<":"^"):""}</span>
+               </th>
+             ))}
+           </tr>
+         ))}
+       </thead>
+       <tbody {...getTableBodyProps()}>
+         {/* {rows are not needed to pagination*/
+         page.map(row => {
+           prepareRow(row)
+           return (
+             <tr {...row.getRowProps()}>
+               {row.cells.map(cell => {
+                 return (
+                   <td
+                     {...cell.getCellProps()}
+                     
+                   >
+                     {cell.render('Cell')}
+                   </td>
+                 )
+               })}
+             </tr>
+           )
+         })}
+       </tbody>
+       <tfoot >
+          { footerGroups.map(footerGroup=>(
+            <tr {...footerGroup.getFooterGroupProps()}>
+              {footerGroup.headers.map(column=>(
+                <td {...column.getFooterProps}> {column.render('Footer')}</td>
+              ))}
+            </tr>
+          ))}
+       </tfoot>
+     </table>
+     
+     <div>
+         <button onClick={()=>{previousPage()}} disabled={!canPreviousPage}>Previous</button>
+         <span>
+            Page {''}
+            <strong>
+                {pageIndex+ 1}of {pageOptions.length}
+            </strong>{''}
+         </span>
+         <button onClick={()=>{nextPage()}} disabled={!canNextPage}>Next</button>
+     </div>
+     </>
+   )
+ }
+ export default Pagination;
